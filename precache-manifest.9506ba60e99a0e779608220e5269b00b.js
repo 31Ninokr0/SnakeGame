51f3f2479566048d2c4d2a86306a9830d27ec1d2
31Ninokr0 <karimelnino69@gmail.com>
2019-09-17 14:54:20 -0400

    Updates

---
+++ precache-manifest.9506ba60e99a0e779608220e5269b00b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e13451e87de77d292e83c71afebf91f1",
+    "url": "/SnakeGame/index.html"
+  },
+  {
+    "revision": "86ba2121403a8f882f46",
+    "url": "/SnakeGame/static/css/main.33e683c6.chunk.css"
+  },
+  {
+    "revision": "2249d86949b54dc0e89e",
+    "url": "/SnakeGame/static/js/2.09aed800.chunk.js"
+  },
+  {
+    "revision": "86ba2121403a8f882f46",
+    "url": "/SnakeGame/static/js/main.c49edf33.chunk.js"
+  },
+  {
+    "revision": "7f48931fc97ebe4e94f2",
+    "url": "/SnakeGame/static/js/runtime~main.86c35b2d.js"
+  },
+  {
+    "revision": "e683f02241ba0a55de5b2aa82c4a4d71",
+    "url": "/SnakeGame/static/media/red-apple.e683f022.png"
+  },
+  {
+    "revision": "a05f1fe37121a2c343b500c3891cb6f8",
+    "url": "/SnakeGame/static/media/snake-head.a05f1fe3.png"
+  }
+]);
